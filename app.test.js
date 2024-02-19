ce5ea7adf648b402b5f21e875d131a468c2c27aa
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-02-19 07:28:34 +0300

    testing

--- app.test.js
+++ app.test.js
@@ -1,2 +1,13 @@
 const superTest = require('supertest');
 const app = require('./app')
+
+describe ("POST /users", ()=>{
+
+    describe("given a username and password", ()=>{
+
+    })
+
+    describe("when the username and password is missing", ()=>{
+        
+    })
+})
