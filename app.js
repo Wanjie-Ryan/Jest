11f80a3b3cd51c666d51edff6f52804ace01561f
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-02-19 09:44:16 +0300

    tests

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const app = express();
 
+function App() {
+  const app = express();
 
+  app.post("/users", (req, res) => {
+    res.sendStatus(200).send({})
+  });
+}
 
-app.post("/users", (req, res) => {
-
-
-
-});
-
-export default app;
+module.exports = App;
--- app.test.js
+++ app.test.js
@@ -1,13 +1,39 @@
-const superTest = require('supertest');
-const app = require('./app')
+const superTest = require("supertest");
+const app = require("./app");
 
-describe ("POST /users", ()=>{
+describe("POST /users", () => {
+  describe("given a username and password", () => {
+    //should save the username and password to the DB
+    // should respond with a json object containing the userid
 
-    describe("given a username and password", ()=>{
+    // should respond with a 200 status code
 
-    })
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/users").send({
+        username: "username",
+        password: "password",
+      });
 
-    describe("when the username and password is missing", ()=>{
-        
-    })
-})
+      console.log(response);
+
+      expect(response.statusCode).toBe(200);
+    });
+
+    // should specify the json in the content type header
+
+    test("Should specify json in the content type header", async () => {
+      const response = await request(app).post("/users").send({
+        username: "username",
+        password: "password",
+      });
+
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      );
+    });
+  });
+
+//   describe("when the username and password is missing", () => {
+//     // should respond with a status code of 400
+//   });
+});
